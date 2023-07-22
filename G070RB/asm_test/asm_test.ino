1474124a545c16b0394607e67ad588ca380ea40c
numeru55 <89648361+numeru55@users.noreply.github.com>
2023-07-23 07:59:30 +0900

    .

---
+++ G070RB/asm_test/asm_test.ino
@@ -0,0 +1,38 @@
+// http://www.neko.ne.jp/~freewing/cpu/stm32_stm32f103c8t6_gcc_inline_assembler/
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial) {}
+  Serial.println("Boot NUCLEO G070");
+
+  int a=1, b=2, c=3; 
+
+  Serial.print("a = ");
+  Serial.print(a);
+  Serial.print(" b = ");
+  Serial.print(b);
+  Serial.print(" c = ");
+  Serial.println(c);
+
+  // res = i + j;
+  asm volatile (
+     "ADD %[result], %[input_i], %[input_j]"
+     : [result] "=r" (a)
+     : [input_i] "r" (b), [input_j] "r" (c)
+     );
+
+  Serial.print("a = ");
+  Serial.print(a);
+  Serial.print(" b = ");
+  Serial.print(b);
+  Serial.print(" c = ");
+  Serial.println(c);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+
