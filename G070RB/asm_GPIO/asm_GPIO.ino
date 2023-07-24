2fd4866a3109a17c8eb0b50423a4a9575f668531
numeru55 <89648361+numeru55@users.noreply.github.com>
2023-07-24 22:41:45 +0900

    .

---
+++ G070RB/asm_GPIO/asm_GPIO.ino
@@ -0,0 +1,25 @@
+// http://www.neko.ne.jp/~freewing/cpu/stm32_stm32f103c8t6_gcc_inline_assembler/
+// https://blueeyes.sakura.ne.jp/2018/01/23/825/
+
+// Nucleo 64
+// PA5 - LED - GND  H:on L:off
+// PC13 - SW with on board pullup   H:open L:close
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial) {}
+  Serial.println("Boot NUCLEO G070");
+
+asm volatile(
+        "eor %[odr], %[odr], #(1 << 5)"
+        : [odr] "+#"(GPIOA->ODR)
+);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+
