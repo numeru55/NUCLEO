2920a366611d25e7a4e5e6d028ef5b1a5b647c55
numeru55 <89648361+numeru55@users.noreply.github.com>
2023-07-26 23:41:07 +0900

    .

--- G070RB/asm_blink/asm_blink.ino
+++ G070RB/asm_blink/asm_blink.ino
@@ -6,25 +6,44 @@
 // PC13 - SW with on board pullup   H:open L:close
 
 void setup() {
+
+  // Serial.begin(9600);
+  // while (!Serial) {}
+  // Serial.println("Boot NUCLEO G070");
+
   pinMode(PA5, OUTPUT);
+
+}
+
+void loop() {
 
   // Set PA5 (to H)
 
   const uint64_t bsrr = GPIOA_BASE + 0x18UL;
 
   asm volatile(
-    ".equ GPIOA_BSRR, 0x50000018 \r\n"
-    "mov r0, #(GPIOA_BSRR) \r\n"
     "mov r1, #(1 << 5) \r\n"
-    "str r1, [r0] \r\n"
+    "str r1, [%[bsrr]] \r\n"
     : 
-    :
-    : "r0", "r1"
+    : [bsrr] "r"(bsrr)
+    : "r1"
     );
 
-}
 
-void loop() {
+  delay(1000);
 
+  // Reset PA5 (to L)
+
+  const uint64_t brr = GPIOA_BASE + 0x28UL;
+
+  asm volatile(
+    "mov r1, #(1 << 5) \r\n"
+    "str r1, [%[brr]] \r\n"
+    : 
+    : [brr] "r"(brr)
+    : "r1"
+    );
+
+  delay(200);
 
 }
