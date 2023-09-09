5f7bc47b4169660e9879d0d57251f07d3a52ea97
numeru55 <89648361+numeru55@users.noreply.github.com>
2023-09-09 09:31:57 +0900

    .

---
+++ G070RB/pio/u8g2/src/main.c
@@ -0,0 +1,28 @@
+#include "stm32g0xx_hal.h"
+
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+int main(void)
+{
+  HAL_Init();
+
+  // init PA5
+
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  GPIO_InitTypeDef GPIO_InitStruct;
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  //GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  while (1)
+  {
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    HAL_Delay(500);
+  }
+}
+
